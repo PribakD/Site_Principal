62aa1896ef4f8f79934a085daf9cb6fef1b2a20b
Pribak <dorian.pribak@gmail.com>
2024-11-25 16:48:10 +0100

    v2.0.1

--- TP_devweb/TP_js3/js3.js
+++ TP_devweb/TP_js3/js3.js
@@ -22,3 +22,61 @@ function seconnecter(){
     {document.write("Bienvenue : "+login);}
     else {alert("Accès refusé")}
 }
+
+function seconnecter2(){
+    window.location.href="identification.html"
+}
+
+function seconnecter3(){
+    i=0;
+    do{
+    var login=prompt("Donnez votre nom d'utilisateur");
+    var password=prompt("Entrez votre mot de passe");
+    
+    if (login=="admin" && password=="admin")
+        {document.write("Bienvenue : "+login);
+        break;}
+    else {alert("Accès refusé");
+        i+=1;}
+    }
+    while(i<3)
+    if (i==3){alert("Délai dépassé")}
+}
+
+function cdc(){
+    var chaine=prompt("Donner un mot");
+    document.write(chaine.toUpperCase()+"<br>");
+    document.write(chaine.toLowerCase()+"<br>");
+    document.write("La chaine contient "+chaine.length+" caracteres "+"<br>");
+    document.write("La première lettre est "+chaine.substr(0,1)+"<br>");
+}
+
+function ligne()
+{window.location.href="ajout.html"}
+
+function ajouterLigne(){
+    var a=document.getElementById("t1").value;
+    var b=document.getElementById("t2").value;
+    if(a==""||b==""){alert("Un ou 2 champs vide");}
+    else if((Number(b)<10||Number(b)>20)){alert("Âge invalide");}
+    else{
+        var table=document.getElementById("myTable");
+        var newRow=table.insertRow(-1);
+        var cell1=newRow.insertCell(0);
+        var cell2=newRow.insertCell(1);
+        cell1.innerHTML=document.getElementById("t1").value;
+        cell2.innerHTML=document.getElementById("t2").value;
+    }
+    
+}
+
+function retour()
+{window.location.href="../../index.html"}
+
+function login(){
+    var login=document.getElementById("t1").value;
+    var mdp=document.getElementById("t2").value;
+    if (login=="admin" && mdp=="admin")
+        {window.location.href="yes.html"}
+    else {window.location.href="non.html"}
+}

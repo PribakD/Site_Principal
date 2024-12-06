7711f8a5825603241a44e52e7d6421ad0f79d165
Pribak <dorian.pribak@gmail.com>
2024-12-06 17:04:13 +0100

    v2.0.7

---
+++ AP/Mission10/script.js
@@ -0,0 +1,10 @@
+function ajouterLigne(){
+    var newLi = document.getElementById("modeleligne").cloneNode(true);
+    var lastLi = document.getElementById("lastLigne");
+}
+
+function calculate(){
+    var listePrix = document.getElementsByClassName("Prix");
+    var listeQte = document.getElementsByClassName("qte");
+    var listeTtl = document.getElementsByClassName("totalLigne");
+}

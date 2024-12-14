a123ad6d9be624ac7d39df305d72522129d93353
Pribak <dorian.pribak@gmail.com>
2024-12-14 13:05:29 +0100

    v2.0.8

--- AP/Mission9/script.js
+++ AP/Mission9/script.js
@@ -1,3 +1,21 @@
 function testqcm(){
-    
+    var score = 0;
+            const questions = document.querySelectorAll('fieldset');
+
+            questions.forEach((question) => {
+                const radio = question.querySelector('input[type="radio"]:checked');
+                if (radio && radio.getAttribute('data-correct') === 'true') {
+                    score++;
+                }
+            });
+            document.getElementById('result').textContent = `Votre score est ${score}`;
+}
+function corrige(){
+    window.open("corrige.html")
+}
+function reinitialise_quest(){
+    document.getElementById("qcm").reset()
+}
+function fermer_page(){
+    window.close()
 }
